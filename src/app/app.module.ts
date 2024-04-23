50c97afa7d22501c154231e32bc35302b9609957
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-04-24 00:54:44 +0700

    explain code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  // dependency injection
+  // By declaring and injecting the httpClient property in the constructor, 
+  // Angular automatically creates and assigns an instance of HttpClient to that property
+  //  when creating an instance of the ProductService class.
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+// represent the structure of the response received from the server
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
