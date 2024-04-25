70be138d5539ad54d272d3feabb33dd9fddc8dbc
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-04-25 23:02:18 +0700

    getProductList

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
 
+  ngOnInit(): void {
+    this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
